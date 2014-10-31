90bd43aed5d9d2e01b12de0d28840b4a8ee9a2c2
Jonathan Minton <nate.minton@gmail.com>
2014-10-31 09:05:00 +0000

    add code to handle year_startend and year_start-end formats

--- script.r
+++ script.r
@@ -405,9 +405,208 @@ l_ply(names(tables_year_month_to_year), fn, .progress="text")
 
 
 
-########################################
+######################################################################
+# So, how many files have I accessed?
+table(file_df$period_structure)
+
+# year : 491
+# year_month : 27
+# year_ 373 : 373
+
+# so, 81% of the tables
 
 
+# With year_startend and year_start_end period structures covered
+# I now have 99.3% coverage
+
+# Now to look at year_startend and year_start-end
+
+
+#year_startend
+file_df_year_startend <- subset(
+  file_df,
+  subset=period_structure=="year_startend"
+)
+
+
+fn_year_startend <- function(x, dir_loc){
+  
+  this_table_name <- x$table_name[1]
+  this_table_name <- tolower(this_table_name)
+  this_table_name <- str_replace_all(this_table_name, " ", "_")
+  fn_inner <- function(xx, dir_loc_){
+    
+    this_filename <- xx$full_filename[1]
+    this_year_range <- xx$table_period[1]
+    this_year_first <- as.numeric(
+      str_sub(this_year_range, 1,4)
+      )
+    
+    this_year_last <- as.numeric(
+      str_sub(this_year_range, 5,8)
+      )
+    
+    
+        
+    dta <- read.csv(
+      paste(
+        dir_loc_,
+        this_filename,
+        sep="/"
+      ),
+      header=T
+    )
+    
+    dta <- dta[-1,]
+    names(dta[1]) <- "datazone"
+    dta$year_first <- this_year_first
+    dta$year_last <- this_year_last
+    dta$year <- this_year_first + ((this_year_last - this_year_first)/2)    
+    dta[,-1] <- apply(dta[,-1], 2, as.numeric)
+    return(dta)      
+  }
+  
+  assign(
+    this_table_name, 
+    ddply(x, .(table_period), fn_inner, dir_loc_=dir_loc)
+  )     
+  return(get(this_table_name))
+}
+
+
+tables_year_startend <- dlply(
+  file_df_year_startend,
+  .(table_name),
+  fn_year_startend,
+  dir_loc=sns_dir,
+  .progress="text"
+)
+
+tables_year_startend <- llply(tables_year_startend, remove.vars, names="table_period", info=F)
+tables_year_startend <- llply(tables_year_startend, rename.vars, from="X", to="datazone", info=F)
+tables_year_startend <- llply(tables_year_startend, arrange, year, datazone)
+
+
+names(tables_year_startend) <- tolower(
+  str_replace_all(
+    names(tables_year_startend),  " ", "_"
+  )
+)
+
+# Spool to csv files
+
+fn <- function(this_table_name){
+  this_table <- tables_year_startend[[this_table_name]]
+  
+  write.csv(
+    this_table,
+    file=paste0(
+      "output_data/by_year_startend/",
+      this_table_name,
+      ".csv"
+    ),
+    row.names=F
+  )
+}
+
+l_ply(names(tables_year_startend), fn, .progress="text")
+
+
+
+#################
+#year_start_end
+file_df_year_start_end <- subset(
+  file_df,
+  subset=period_structure=="year_start-end"
+)
+
+
+fn_year_start_end <- function(x, dir_loc){
+  
+  this_table_name <- x$table_name[1]
+  this_table_name <- tolower(this_table_name)
+  this_table_name <- str_replace_all(this_table_name, " ", "_")
+  fn_inner <- function(xx, dir_loc_){
+    
+    this_filename <- xx$full_filename[1]
+    this_year_range <- xx$table_period[1]
+    tmp <- str_split(this_year_range, "-")
+    this_year_first <- as.numeric(tmp[[1]][1])
+    this_year_last <- as.numeric(tmp[[1]][2])
+
+    
+    
+    
+    dta <- read.csv(
+      paste(
+        dir_loc_,
+        this_filename,
+        sep="/"
+      ),
+      header=T
+    )
+    
+    dta <- dta[-1,]
+    names(dta[1]) <- "datazone"
+    dta$year_first <- this_year_first
+    dta$year_last <- this_year_last
+    dta$year <- this_year_first + ((this_year_last - this_year_first)/2)    
+    dta[,-1] <- apply(dta[,-1], 2, as.numeric)
+    return(dta)      
+  }
+  
+  assign(
+    this_table_name, 
+    ddply(x, .(table_period), fn_inner, dir_loc_=dir_loc)
+  )     
+  return(get(this_table_name))
+}
+
+
+tables_year_start_end <- dlply(
+  file_df_year_start_end,
+  .(table_name),
+  fn_year_start_end,
+  dir_loc=sns_dir,
+  .progress="text"
+)
+
+tables_year_start_end <- llply(tables_year_start_end, remove.vars, names="table_period", info=F)
+tables_year_start_end <- llply(tables_year_start_end, rename.vars, from="X", to="datazone", info=F)
+tables_year_start_end <- llply(tables_year_start_end, arrange, year, datazone)
+
+
+names(tables_year_start_end) <- tolower(
+  str_replace_all(
+    names(tables_year_start_end),  " ", "_"
+  )
+)
+
+# Spool to csv files
+
+fn <- function(this_table_name){
+  this_table <- tables_year_start_end[[this_table_name]]
+  
+  write.csv(
+    this_table,
+    file=paste0(
+      "output_data/by_year_start_end/",
+      this_table_name,
+      ".csv"
+    ),
+    row.names=F
+  )
+}
+
+l_ply(names(tables_year_start_end), fn, .progress="text")
+
+
+################################################################
+
+
+
+
+#################################################################
 # Want to process separately by structure of period variables
 # First, I want to just find the variables that are years: these 
 # are just four characters long
